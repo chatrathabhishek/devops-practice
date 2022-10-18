5dff847e1b06ebab6e22925ad34bd9b888414f4f
ac185391@ncr.com <ac185391@ncr.com>
2022-10-18 12:59:25 -0400

    Added: argocd

--- jenkins/ArgoCD/script.groovy
+++ jenkins/ArgoCD/script.groovy
@@ -1,11 +1,21 @@
 def installArgoCD() {
     echo "Installing ArgoCD..."
     sh """
-        gcloud container clusters get-credentials ac-cluster-1 --zone us-central1-c --project sws-globalsre-cug01-qa
+        gcloud container clusters get-credentials ${params.cluster} --region ${params.region} --project ${params.projectKey}
         kubectl create namespace argocd
         kubectl label namespace argocd istio-injection=enabled
+        cd jenkins/ArgoCD
         kustomize build kustomize/ | kubectl apply -f -
         kustomize build kustomize/overlays | kubectl apply -f -
+    """
+}
+
+def uninstallArgoCD() {
+    echo "Uninstalling Argo..."
+    sh """
+        gcloud container clusters get-credentials ${params.cluster} --region ${params.region} --project ${params.projectKey}
+        kustomize build kustomize/ | kubectl delete -f -
+        kustomize build kustomize/overlays | kubectl delete -f -
     """
 }
 
